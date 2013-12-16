b224e84e8c7d588f1e2c34bcfb1677526c027eb4
Brandon Murry <brmurry@zappos.com>
2013-12-16 09:34:23 -0800

    fixed source loading and added some boilerplate code for amd support

--- src/delegant.js
+++ src/delegant.js
@@ -3,67 +3,81 @@
  * Written by Brandon Murry
 */
 
-(function(w,d){
-  'use strict';
+/* AMD boilerplate from:
+ * https://gist.github.com/fitzgen/951178
+*/
+(typeof define === "function"
+ ? define
+ : function (_, f) {
+     if ( typeof exports === "object" ) {
+         f(exports);
+     } else {
+         f(this);
+     }
+ })(["exports"], function (exports) {
 
-  var delegant = (function() {
+  (function(w,d){
+    'use strict';
 
-    var
-      evtTarget,
-      evtType,
-      evtObj,
-      fn = {}
-    ;
+    var delegant = (function() {
 
-    function capitalize(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    }
+      var
+        evtTarget,
+        evtType,
+        evtObj,
+        fn = {}
+      ;
 
-    function evtFunc(e) {
-      evtTarget = e.target;
-      evtType = e.type;
-
-      if ((evtObj = evtTarget.dataset['dele' + capitalize(evtType)])) {
-        if ((evtObj.indexOf('.')+1)) {
-          evtObj = evtObj.split('.');
-          evtObj = fn[evtObj[0]][evtObj[1]];
-        } else {
-          evtObj = fn[evtObj];
-        }
-
-        e.preventDefault();
-        evtObj(evtTarget, e);
+      function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
       }
-    }
 
-    delegant = {
-      bind: function(el,evt) {
-        var _this = this;
-        return typeof evt === 'string' ? this.addListener(el,evt)
-          : evt.forEach(function(evt) {
-            _this.addListener(el,evt);
-          });
-      },
+      function evtFunc(e) {
+        evtTarget = e.target;
+        evtType = e.type;
 
-      addListener: function(el,evt) {
-        d.querySelector(el).addEventListener(
-          evt,
-          (function(e){evtFunc(e);}),
-          true
-        );
-      },
+        if ((evtObj = evtTarget.dataset['dele' + capitalize(evtType)])) {
+          if ((evtObj.indexOf('.')+1)) {
+            evtObj = evtObj.split('.');
+            evtObj = fn[evtObj[0]][evtObj[1]];
+          } else {
+            evtObj = fn[evtObj];
+          }
 
-      register: function(name, func) {
-        if ((name.indexOf('.')+1)) {
-          name = name.split('.');
-          fn[name[0]] = fn[name[0]] || {};
-          fn[name[0]][name[1]] = func;
-        } else {
-          fn[name] = func; //store the function into the delegant function object
+          e.preventDefault();
+          evtObj(evtTarget, e);
         }
       }
-    };
 
-    return w.delegant = delegant;
-  }());
-}(window, document));
+      delegant = {
+        bind: function(el,evt) {
+          var _this = this;
+          return typeof evt === 'string' ? this.addListener(el,evt)
+            : evt.forEach(function(evt) {
+              _this.addListener(el,evt);
+            });
+        },
+
+        addListener: function(el,evt) {
+          d.querySelector(el).addEventListener(
+            evt,
+            (function(e){evtFunc(e);}),
+            true
+          );
+        },
+
+        register: function(name, func) {
+          if ((name.indexOf('.')+1)) {
+            name = name.split('.');
+            fn[name[0]] = fn[name[0]] || {};
+            fn[name[0]][name[1]] = func;
+          } else {
+            fn[name] = func; //store the function into the delegant function object
+          }
+        }
+      };
+
+      return w.delegant = delegant;
+    }());
+  }(window, document));
+ });
